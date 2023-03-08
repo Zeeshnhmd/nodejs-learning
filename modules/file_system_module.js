cc1d22213aa7d6278d8157eb687c8ef2ec442975
Zeeshnhmd <zeeshnhmd.1@gmail.com>
2023-03-08 18:37:24 +0530

    create a file "writefile"

--- modules/file_system_module.js
+++ modules/file_system_module.js
@@ -7,12 +7,27 @@
 
 const fs = require('fs');
 const path = require('path');
+const color = require('cli-color');
 
 //create a folder
 fs.mkdir(path.join(__dirname, '/test'), (err) => {
 	if (err) {
-		console.log(`Something went wrong ${err}`);
-		return;
+		console.log(color.red(`Something went wrong ${err}`));
+		// return;
 	}
 	console.log('Folder created successfully...');
 });
+
+//create a file
+fs.writeFile(
+	path.join(__dirname, 'test', 'test.txt'),
+	'Hello Node\n',
+	(err) => {
+		if (err) {
+			console.log(color.red(`Something went wrong ${err}`));
+		}
+		console.log(color.green('File created successfully'));
+	}
+);
+
+//when you run the above code again (create file) it not throw an error what it does is that it deletes the existing file and create a new file again.
