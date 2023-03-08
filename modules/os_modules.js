c719c2778f6c82780d6f602a8c03204a04d286b4
Zeeshnhmd <zeeshnhmd.1@gmail.com>
2023-03-08 20:52:11 +0530

    os module: os architecture

--- modules/os_modules.js
+++ modules/os_modules.js
@@ -6,3 +6,6 @@ console.log(color.bgBlue(`OS Type: ${os.type()}`));
 
 // OS platform
 console.log(color.bgMagenta(`OS Platform: ${os.platform}`));
+
+// OS architecture
+console.log(color.bgCyanBright(`OS architecture: ${os.arch()}`));
