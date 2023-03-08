042f38fc9f787ab19a27c7c34cbb5b743e282481
Zeeshnhmd <zeeshnhmd.1@gmail.com>
2023-03-08 15:03:50 +0530

    path modules

--- modules/path_modules.js
+++ modules/path_modules.js
@@ -10,8 +10,9 @@ console.log(`File name: ${color.yellow(path.basename(__filename))}`);
 //extension name
 console.log(`Extension name: ${color.blue(path.extname(__filename))}`);
 
-//parse
+//parse: this give us the whole information of file.
 console.log(`Parse: ${path.parse(__filename)}`);
 
-//join
+//join: used to join things here (__dirname gives us the current file path), ("order" is file name inside the current file) and ("app.js" is file name inside "order")
+//Output: C:\Personal_space\learning\Backend\NodeJS\nodejs-learning\modules\order\app.js
 console.log(`Join: ${color.blue(path.join(__dirname, 'order', 'app.js'))}`);
