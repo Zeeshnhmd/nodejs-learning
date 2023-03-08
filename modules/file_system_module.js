2a524fa9c95e9e1827f8a82435113a40b4d974ed
Zeeshnhmd <zeeshnhmd.1@gmail.com>
2023-03-08 18:17:42 +0530

    create a folder "mkdir"

--- modules/file_system_module.js
+++ modules/file_system_module.js
@@ -4,3 +4,15 @@
 // - Update files
 // - Delete files
 // - Rename files
+
+const fs = require('fs');
+const path = require('path');
+
+//create a folder
+fs.mkdir(path.join(__dirname, '/test'), (err) => {
+	if (err) {
+		console.log(`Something went wrong ${err}`);
+		return;
+	}
+	console.log('Folder created successfully...');
+});
